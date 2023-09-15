2b30eca8515329e1cde2617341524493bead877c
Nathan Hadden <nhadde445@west-mec.org>
2023-09-15 13:53:58 -0700

    First Commit

---
+++ qrgenerator.js
@@ -0,0 +1,31 @@
+const inquirer = require('inquirer');
+
+const qr = require('qr-image');
+
+const fs = require('fs');
+
+inquirer
+    .prompt([
+        {
+            message: "Type in your URL: ",
+            name: "URL"
+        }
+    ])
+    .then((answers) => {
+        const url = answers.URL;
+        var qr_svg = qr.image(url);
+        qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+
+        fs.writeFile("URL.txt", url, (err) => {
+            if(err) throw err;
+            console.log("The file has been saved!");
+        });
+    })
+    
+    .catch((error) => {
+        if(error.isTtyError) {
+
+        } else {
+
+        }
+    })
